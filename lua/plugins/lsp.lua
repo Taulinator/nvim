8cc0a0c396b8c27afd0d93983e4d0139252626b7
Taulinator <Taulinator@users.noreply.github.com>
2022-12-14 12:03:26 +0100

    Neovim cmp update

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -39,7 +39,7 @@ local function config()
         return
     end
 
-    local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+    local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
     lsp.gopls.setup{
         cmd = getContainerCmd( {image = 'gopls', mounts = { vim.fn.getcwd() }} ),
